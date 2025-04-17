48de0d136a9f3721fb51e8824afda50ea240e0f2
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-18 01:33:16 +0500

    Update main.py

--- poem-generator/main.py
+++ poem-generator/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+
 st.title("Random Poem Generator 📝")
 
 if st.button("Generate Poem"):
@@ -7,6 +8,15 @@ if st.button("Generate Poem"):
         response = requests.get("https://poetrydb.org/random")
         if response.status_code == 200:
             data = response.json()
-            print (data)
+            title = data[0]["title"]
+            lines = data[0]["lines"]
+            author = data[0]["author"]
+
+            result = title, lines, author
+            st.success(result)
+            st.success(lines)
+            st.success(author)
+            
+            
     except:
         print("Error found can't load data!")
